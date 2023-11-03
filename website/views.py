4829122280a132e6fc5ee135dd08b80364c1edfb
hessnt30 <hessnt30@vt.edu>
2023-11-02 21:08:03 -0400

    Changes to be committed: 	new file:   app.py 	new file:   instance/database.db 	new file:   website/__init__.py 	new file:   website/__pycache__/__init__.cpython-311.pyc 	new file:   website/__pycache__/auth.cpython-311.pyc 	new file:   website/__pycache__/models.cpython-311.pyc 	new file:   website/__pycache__/views.cpython-311.pyc 	new file:   website/auth.py 	new file:   website/models.py 	new file:   website/static/index.js 	new file:   website/templates/base.html 	new file:   website/templates/create_posts.html 	new file:   website/templates/feed.html 	new file:   website/templates/home.html 	new file:   website/templates/login.html 	new file:   website/templates/post_div.html 	new file:   website/templates/posts.html 	new file:   website/templates/profile.html 	new file:   website/templates/sign_up.html 	new file:   website/views.py

---
+++ website/views.py
@@ -0,0 +1,116 @@
+from flask import Blueprint, render_template, flash, request, redirect, url_for
+from flask_login import login_required, current_user
+from .models import Post, User, Comment, Like
+from . import db
+
+views = Blueprint("views", __name__)
+
+
+@views.route("/")
+@views.route("/home")
+@login_required
+def home():
+    return render_template("home.html", user=current_user)
+
+@views.route("/feed")
+@login_required
+def feed():
+    posts = Post.query.all()
+    return render_template("feed.html", user=current_user, posts=posts)
+
+@views.route("/create-post", methods=['GET', 'POST'])
+@login_required
+def create_post():
+    if request.method == 'POST':
+        text = request.form.get('text')
+
+        if not text:
+            flash('Post cannot be empty.', category='error')
+        else:
+            post = Post(text=text, author=current_user.id)
+            db.session.add(post)
+            db.session.commit()
+            flash('Post created', category='success')
+            return redirect(url_for('views.feed'))
+
+    return render_template("create_posts.html", user=current_user)
+
+@views.route("/delete-post/<post_id>")
+@login_required
+def delete_post(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+
+    if not post:
+        flash("Post does not exist.", category='error')
+    elif current_user.id != post.author:
+        flash('You do not have permission to delete this post.', category='error')
+    else:
+        db.session.delete(post)
+        db.session.commit()
+        flash('Post deleted.', category='success')
+
+    return redirect(url_for("views.feed"))
+
+@views.route("/posts/<username>")
+@login_required
+def posts(username):
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        flash('No such user.', category='error')
+        return redirect(url_for('views.feed'))
+    
+    posts = user.posts
+    return render_template("posts.html", user=current_user, posts=posts, username=username)
+
+@views.route("/create-comment/<post_id>", methods=['POST'])
+@login_required
+def create_comment(post_id):
+    text = request.form.get('text')
+
+    if not text:
+        flash('Comment cannot be empty.', category='error')
+    else:
+        post = Post.query.filter_by(id=post_id)
+        if post:
+            comment = Comment(text=text, author=current_user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+        else:
+            flash('Post does not exist.', category='error')
+
+    return redirect(url_for('views.feed'))
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash('Comment does not exist.', category='error')
+    elif current_user.id != comment.author and current_user != comment.post.author:
+        flash('You are not authorized to delete this comment.', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+    return redirect(url_for("views.feed"))
+
+@views.route("/like-post/<post_id>", methods=['GET'])
+@login_required
+def like(post_id):
+    post = Post.query.filter_by(id=post_id)
+
+    like = Like.query.filter_by(author=current_user.id, post_id=post_id).first()
+
+    if not post:
+        flash('Post does not exist.', category='error')
+    elif like:
+        db.session.delete(like)
+        db.session.commit()
+    else:
+        like = Like(author=current_user.id, post_id=post_id)
+        db.session.add(like)
+        db.session.commit()
+        
+    return redirect(url_for('views.feed'))
